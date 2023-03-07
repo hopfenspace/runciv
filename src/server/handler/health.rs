ab327d22cca72b2c443fa669d002abda9f00d15b
myOmikron <git@omikron.dev>
2023-03-07 02:01:02 +0100

    Added more docs for health endpoint

--- src/server/handler/health.rs
+++ src/server/handler/health.rs
@@ -20,6 +20,9 @@ pub struct HealthResponse {
 }
 
 /// Request health data from this server.
+///
+/// `accounts` are the currently registered user accounts on the server
+/// `connections` are the currently open connections
 #[utoipa::path(
     tag = "Server status",
     context_path = "/api/v2/admin",
